77622ca334f8baeae4741641bbee78696cc31c08
freefis <freefis@gmail.com>
2010-11-10 19:31:54 +0800

    i

---
+++ get_data/blog_rss.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+#-------------------------------------------
+# Auther      : free.Wang<freefis@gmail.com> 
+# Licence     : License on GPL Licence
+# Description :
+#    ...
+# Archieved   : Mar 2nd 2009  
+#-------------------------------------------
+
+import feedparser
+
+result = feedparser.parse("http://feed.feedsky.com/cg87")
+print result
